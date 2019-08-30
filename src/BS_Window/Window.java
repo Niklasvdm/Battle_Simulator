baf30b053a8ef233c993f1d56c74a607aa26da2f
Niklas Van der Mersch <Niklas@vandermersch.eu>
2019-08-30 16:11:26 +0200

    Version 0.12
    -Added implementation for Window
    -Tried to implement simple sprite (src/Resources/Dot.png) but got annoyed and tired of trying to figure out how.

--- src/BS_Window/Window.java
+++ src/BS_Window/Window.java
@@ -1,23 +1,35 @@
+package BS_Window;
 import java.awt.* ;
 import javax.swing.* ;
-import Battle_Simulator/src/* ;
-
 
 /**
  This class is intended to make a simple window, in set dimensions with a given tilesize
+ http://zetcode.com/tutorials/javagamestutorial/movingsprites/ --> for help
 **/
 
-public class Window(int size) {
-   public static void Window(int size) {
-        JFrame frame = new JFrame("FrameDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
-            }
+public class Window {
+    public static void main(String[] args) throws HeadlessException{
+        Window(5);
+    }
 
-    public static void class main() {
-        Window wuwu = new Window(5);
-            }
+    static GraphicsConfiguration gc;
 
-        }
+   public static void Window(int size) throws HeadlessException {
+
+       JFrame frame = new JFrame(gc);
+       frame.setTitle("Hello World!");
+       frame.setSize(600   ,1000);
+       frame.setLocation(600,0);
+/*       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+       frame.pack();*/
+       frame.setVisible(true);
+/*       Graphics2D Graph = (Graphics2D) g;
+       ImageIcon imago = new ImageIcon("src/Resources/Dot.png");
+       image = imago.getImage();
+       Graph.drawImage();*/
+   }
+
+
+
+}
--- src/Controller.java
+++
@@ -1,5 +0,0 @@
-import Battle_Simulator/src/BS_Window/* ;
-import javax.swing.* ;
-import Battle_Simulator/src/* ;
-
-
